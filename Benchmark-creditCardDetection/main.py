cb4f5f076720fdaefaba9343fbf8a29e5cda0f7b
Rafayel Latif <rafayel.latif@gmail.com>
2025-05-30 20:45:43 +0100

    Intial tests

---
+++ Benchmark-creditCardDetection/main.py
@@ -0,0 +1,71 @@
+import kagglehub
+import pandas as pd
+import ao_core as ao
+import ao_embeddings.binaryEmbeddings as be
+from config import OPENAI_KEY
+import numpy as np
+
+Arch = ao.Arch(arch_i=[20, 20], arch_z=[1])
+Agent = ao.Agent(Arch=Arch)
+
+embeddingToBinary = be.binaryEmbeddings(OPENAI_KEY, numberBinaryDigits=20)
+
+# This seems like the best dataset to use since it is pretty balenced
+
+# Download latest version
+path = kagglehub.dataset_download("nelgiriyewithana/credit-card-fraud-detection-dataset-2023")
+
+# Load the dataset
+df = pd.read_csv(path+ "/creditcard_2023.csv")
+# Need some way to reduce the dimensaonality of the dataset 
+
+# Vs 0-28 are anonymized features such as time, location, and other transaction details. I am going to be refering to v1 to v28 as feature embeddings.
+
+"""
+    id        V1        V2        V3        V4        V5        V6        V7        V8        V9       V10  ...       V20       V21       V22       V23       V24       V25       V26       V27       V28    Amount  Class
+0   0 -0.260648 -0.469648  2.496266 -0.083724  0.129681  0.732898  0.519014 -0.130006  0.727159  0.637735  ...  0.091202 -0.110552  0.217606 -0.134794  0.165959  0.126280 -0.434824 -0.081230 -0.151045  17982.10      0
+1   1  0.985100 -0.356045  0.558056 -0.429654  0.277140  0.428605  0.406466 -0.133118  0.347452  0.529808  ... -0.233984 -0.194936 -0.605761  0.079469 -0.577395  0.190090  0.296503 -0.248052 -0.064512   6531.37      0
+2   2 -0.260272 -0.949385  1.728538 -0.457986  0.074062  1.419481  0.743511 -0.095576 -0.261297  0.690708  ...  0.361652 -0.005020  0.702906  0.945045 -1.154666 -0.605564 -0.312895 -0.300258 -0.244718   2513.54      0
+3   3 -0.152152 -0.508959  1.746840 -1.090178  0.249486  1.143312  0.518269 -0.065130 -0.205698  0.575231  ... -0.378223 -0.146927 -0.038212 -0.214048 -1.893131  1.003963 -0.515950 -0.165316  0.048424   5384.44      0
+4   4 -0.206820 -0.165280  1.527053 -0.448293  0.106125  0.530549  0.658849 -0.212660  1.049921  0.968046  ...  0.247237 -0.106984  0.729727 -0.161666  0.312561 -0.414116  1.071126  0.023712  0.419117  14278.97      0
+"""
+
+# shuffle the dataset
+
+
+def convertAmountToBinary(amount):
+    binary_amount = format(int(amount), "020b")
+    return binary_amount
+
+df = df.sample(frac=1).reset_index()
+
+correct = 0
+
+for row in df[0:100].iterrows():
+    amount  = row[1]["Amount"]
+    class_type = row[1]["Class"]
+    feature_embedding  = row[1][1:29].values  # V1 to V28
+
+    binary_embedding = embeddingToBinary.embeddingToBinary(feature_embedding)
+    binary_amount = convertAmountToBinary(amount)
+    binary_amount_list = []
+    
+    for bit in binary_amount:
+        binary_amount_list.append(int(bit))
+
+    binary_amount = binary_amount_list  # Convert string to list of integers
+
+    input_data = np.append(binary_embedding, binary_amount)  # Combine the binary embedding with the binary amount
+
+
+    response = Agent.next_state(INPUT=input_data)
+
+    if response[0] == class_type:
+        Agent.next_state(INPUT=input_data, LABEL=response)
+        correct+=1
+    else:
+        Agent.next_state(INPUT=input_data, LABEL=[abs(1-response[0])])  # If the response is not equal to the class type, then we will label it as the opposite class
+
+    # Will be doing a Guassian random projection to reduce the dimensionality of the feature embeddings
+
+print("amount correct: ", correct/100)
--- Identra-reumes/ResumeDetection.py
+++ Identra-reumes/ResumeDetection.py
@@ -1,7 +1,9 @@
 from pypdf import PdfReader
-
+import os
 
 root = "resume_samples/"
+
+files = os.listdir(root)
 
 def extract_text_from_pdf(pdf_path):
     reader = PdfReader(pdf_path)
@@ -12,5 +14,10 @@ def extract_text_from_pdf(pdf_path):
         text += page.extract_text() + "\n"
     return text.strip()
 
-text_from_resume = extract_text_from_pdf(f"{root}1Amy.pdf")
-print(text_from_resume)
+resumes = []
+for f in files:
+    if f.endswith('.pdf'):
+        text_from_resume = extract_text_from_pdf(f"{root}{f}")
+        resumes.append(text_from_resume)
+
+        
